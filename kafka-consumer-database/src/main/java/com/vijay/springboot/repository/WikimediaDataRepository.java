a2a4a32bb2e953925165e6e67336f545b880bb0f
Vijay Pandey <vijaypandey@Vijays-MacBook-Air.local>
2023-08-06 12:39:00 +0530

    Springboot kafka project to read recent changes data from wikimedi API and writing it to DB

---
+++ kafka-consumer-database/src/main/java/com/vijay/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package com.vijay.springboot.repository;
+
+import com.vijay.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
